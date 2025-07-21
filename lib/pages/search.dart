ee18e0e3930de1f09def6fd28e8ad30108897eef
optus-56 <bijanbakabal45@gmail.com>
2025-07-21 17:48:25 +0545

    added image picking feature

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,7 +1,36 @@
+import 'dart:io'; 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+
+  File? _selectedImage;
+  final TextEditingController _textController = TextEditingController();
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _selectedImage = File(pickedFile.path);
+        _textController.clear();
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,20 +46,19 @@ class SearchScreen extends StatelessWidget {
       floatingActionButton: Container(
         margin: EdgeInsets.only(left: 20),
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             SizedBox(width: 20),
-            SizedBox(
-              height: 50,
+            Container(
+              height: 65,
               width: 60,
-              child: FloatingActionButton(
+              decoration: BoxDecoration(
+                color: Color(0xFF2B2D30),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: IconButton(
                 onPressed: () {},
-                backgroundColor: Color(0xFF2B2D30),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: SvgPicture.asset(
-                  'assets/icons/VoiceSearch.svg',
-                ),
+                icon: SvgPicture.asset('assets/icons/VoiceSearch.svg'),
               ),
             ),
 
@@ -38,8 +66,11 @@ class SearchScreen extends StatelessWidget {
 
             Expanded(
               child: SizedBox(
-                height: 50,
+                height: 65,
                 child: TextField(
+                  controller: _textController,
+                  readOnly: _selectedImage != null,
+                  showCursor: _selectedImage == null,
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 12,
@@ -47,27 +78,86 @@ class SearchScreen extends StatelessWidget {
                   decoration: InputDecoration(
                     filled: true,
                     fillColor: Color(0xFF2B2D30),
-                    hintText: 'What are we looking up for today',
+                    hintText: _selectedImage == null ? 'What are we looking up for today' : null,
                     hintStyle: TextStyle(
                       color: Colors.white70,
                       fontSize: 12,
                     ),
-                    contentPadding: EdgeInsets.symmetric(horizontal: 16),
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 22
+                      ),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(15),
                       borderSide: BorderSide.none,
                     ),
-                    suffixIcon: Padding(
-                      padding: EdgeInsets.only(right: 8),
-                      child: IconButton(
-                        onPressed: () {
-                
-                        },
-                        icon: SvgPicture.asset(
-                          'assets/icons/SendIcon.svg',
+
+                    prefixIcon: _selectedImage != null
+                      ? Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                        child: Stack(
+                          clipBehavior: Clip.none,
+                          children: [
+                            SizedBox(
+                              width: 36,
+                              height: 36,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(6),
+                                child: Image.file(
+                                  _selectedImage!,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              top: -6,
+                              right: -6,
+                              child: GestureDetector(
+                                onTap: () {
+                                setState(() {
+                                  _selectedImage = null;
+                                });
+                                },
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                  color: Colors.black.withOpacity(0.7),
+                                  shape: BoxShape.circle,
+                                  ),
+                                  padding: EdgeInsets.all(2),
+                                  child: Icon(
+                                    Icons.close,
+                                    size: 12,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                        padding: EdgeInsets.zero,
-                        constraints: BoxConstraints(),
+                      )
+                    : null,
+
+
+                    suffixIcon: Padding(
+                      padding: EdgeInsets.only(right: 3),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            onPressed: _pickImage,
+                            icon: Icon(Icons.image, color: Colors.white),
+                            padding: EdgeInsets.zero,
+                            constraints: BoxConstraints(),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: SvgPicture.asset(
+                              'assets/icons/SendIcon.svg',
+                            ),
+                            padding: EdgeInsets.zero,
+                            constraints: BoxConstraints(),
+                          ),
+                        ],
                       ),
                     ),
                   ),
